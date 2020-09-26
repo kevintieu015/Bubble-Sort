c6d8114237e071a43a0e11e4ea45bab06547019f
kevint-65 <kevint6565@gmail.com>
2020-09-26 13:03:50 -0700

    Bubble Sort done

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 public class bubbleSort {
 
 	void bubbleSort(int array[]) {
-		//Need double for loop 
+		// Need double for loop
 		for (int i = 0; i < array.length - 1; i++) {
 			for (int j = 0; j < array.length - 1; j++) {
 				if (array[j] > array[j + 1]) {
 
-					//swapping arrays
+					// swapping arrays
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
@@ -17,6 +17,18 @@ public class bubbleSort {
 			}
 		}
 	}
+	public static void main(String args[]){
+		int array[] = new int[5];
+		
+		for (int i = 0; i < array.length; i++){
+			array[i] = (int)(Math.random()*100);
+		}
+		
+		System.out.println("UnSorted array: " + Arrays.toString(array));
 
-	
+		bubbleSort object = new bubbleSort();
+		object.bubbleSort(array);
+		System.out.println("Sorted array: " + Arrays.toString(array));
+	}
+
 }
